5e078f9236493acb750c4188e919639103588d9c
sohan <sohanreddy06@gmail.com>
2026-02-23 23:12:00 +0530

    added optimal soln

--- arrays/medium/largest_consec_seq_array.cpp
+++ arrays/medium/largest_consec_seq_array.cpp
@@ -17,8 +17,28 @@ int brute_force(vector<int>&arr){
     }
     return max_count+1;
 }
+int optimal_soln(vector<int>&arr){
+    if(arr.empty())return 0;
+    int longest=1;
+    unordered_set<int>st;
+    for(auto it:arr){
+        st.insert(it);
+    }
+    for(auto it:st){
+        if(st.find(it-1)==st.end()){
+            int count=1;
+            int x=it;
+            while(st.find(x+1)!=st.end()){
+                x+=1;
+                count+=1;
+            }
+            longest=max(longest,count);
+        }
+    }
+    return longest;
+}
 int main()
 {
     vector<int>arr={100, 4, 200, 1, 3, 2};
-    cout<<brute_force(arr);
+    cout<<optimal_soln(arr);
 }
