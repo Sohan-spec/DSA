9174a51431c952495bf3eda4a587308aa790b0c4
sohan <sohanreddy06@gmail.com>
2026-03-09 09:22:03 +0530

    added optimal solution

--- arrays/binary_search_in_arrays/koko_eating_bananas.cpp
+++ arrays/binary_search_in_arrays/koko_eating_bananas.cpp
@@ -13,8 +13,26 @@ int brute_force(vector<int>&arr,int hrs){
     }
     return maxe;
 }
+int optimal_soln(vector<int>&arr,int hrs){
+    int maxe=*max_element(arr.begin(),arr.end());
+    int low=1,high=maxe;
+    while(low<=high){
+        int mid=(low+high)/2;
+        int sum=0;
+        for(int i=0;i<arr.size();i++){
+            sum+=ceil((double)arr[i]/(double)mid);
+        }
+        if(sum<=hrs){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return low;
+}
 int main()
 {
     vector<int>arr={3,6,7,11};
-    cout<<brute_force(arr,8);
+    cout<<optimal_soln(arr,8);
 }
