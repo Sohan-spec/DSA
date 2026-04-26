f3509e7ee4b0a7643f6db66e945d39886d902eec
Sohan Reddy <sohanreddy06@gmail.com>
2026-04-26 16:12:37 +0530

    did shuffle indices

---
+++ strings/shuffle_string.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+string brute_force(string s, vector<int>&indices){
+    unordered_map<int,char>mpp;
+    for(int i=0;i<indices.size();i++){
+        mpp[indices[i]]=s[i];
+    }
+    for(int i=0;i<indices.size();i++){
+        s[i]=mpp[i];
+    }
+    return s;
+}
+int main()
+{
+    vector<int>indices={4,5,6,7,0,2,1,3};
+    cout<<brute_force("codeleet",indices);
+}
